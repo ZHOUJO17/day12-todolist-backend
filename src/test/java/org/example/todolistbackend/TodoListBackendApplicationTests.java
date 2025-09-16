a3a54ede0183e031c9c348cb130f115347e38014
Jonny Zhou <jonny.zhou@oocl.com>
2025-09-16 15:16:22 +0800

    feat:init

---
+++ src/test/java/org/example/todolistbackend/TodoListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.todolistbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
